d64dab74352ce6b856483ca1727cb47d353943a7
jrodriguezp3 <jrodriguezp3@uteq.edu.ec>
2025-06-09 17:54:01 -0500

    Initial commit of InterfazGrfica project

---
+++ app/src/main/java/com/example/interfazgrfica/longActivity2.java
@@ -0,0 +1,38 @@
+package com.example.interfazgrfica;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class longActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activitylong2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+    public void btLogin(View v){
+        TextInputLayout ilUsuario = findViewById(R.id.textInputLayout2);
+        TextInputEditText edtUsr = findViewById(R.id.txtUsuario);
+        if(edtUsr.getText().toString().isEmpty()){
+            ilUsuario.setError("Nombre requerido");
+        }else{
+            ilUsuario.setErrorEnabled(false);
+        }
+    }
+}
